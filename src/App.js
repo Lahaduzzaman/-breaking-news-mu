dba583bd9980fe0c6d6a3a02fdddb09f2f14769d
Lahaduzzaman <criclahad.2@gmail.com>
2021-03-04 15:28:46 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+// import { Button } from '@material-ui/core';
 import './App.css';
+import News from './components/News/News';
+import { useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  useEffect( () => {
+    const url = 'http://newsapi.org/v2/everything?q=tesla&from=2021-02-03&sortBy=publishedAt&apiKey=80c696efd72d484ea401955fb2afc8f2';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, [])
+  useEffect( () =>{
+    const url = 'http://newsapi.org/v2/everything?q=tesla&from=2021-02-03&sortBy=publishedAt&apiKey=80c696efd72d484ea401955fb2afc8f2';
+    axios(url)
+    .then(data => setArticles(data.data.articles))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Headlines: {articles.length}</h2>
+      {
+        articles.map(article => <News article = {article}></News>)
+      }
     </div>
   );
 }
